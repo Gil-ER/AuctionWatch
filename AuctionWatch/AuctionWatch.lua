6db03c457d3aeec1d0f7c732fad807d648f1c36d
Gil-ER <90463047+Gil-ER@users.noreply.github.com>
2022-05-26 23:54:17 -0400

    Revert "code cleaning"
    This reverts commit 03d8ae6a357159957169ce6dfff9ba91c3d2cf4e.

---
+++ AuctionWatch/AuctionWatch.lua
@@ -0,0 +1,76 @@
+--aw namespace variable
+local _, aw = ...;
+
+aw.ID = GetUnitName("player") .. "-" .. GetRealmName();
+aw.auctionCount = 0;
+
+SLASH_AUCTIONWATCH1 = "/auctionwatch";
+SLASH_AUCTIONWATCH2 = "/aw";
+SlashCmdList.AUCTIONWATCH = function(msg)
+	aw:ReportAuctionsToWindow();
+end
+
+
+-- **********************************************************************
+-- Event Frame
+-- **********************************************************************
+local frame = CreateFrame("FRAME");
+frame:RegisterEvent("SPELLS_CHANGED");
+frame:RegisterEvent("PLAYER_LOGIN");
+frame:RegisterEvent("AUCTION_HOUSE_CLOSED");
+frame:RegisterEvent("OWNED_AUCTIONS_UPDATED");
+frame:RegisterEvent("AUCTION_HOUSE_AUCTION_CREATED");
+
+function frame:OnEvent(event, arg1, arg2)
+	if event == "SPELLS_CHANGED" then
+		--Play Raid Warning if there are really old auctions
+		if aw:VeryOldAuctions() then
+			PlaySound(SOUNDKIT.RAID_WARNING);
+			aw:myPrint(aw:colorString("red", "*******************************************") );
+			aw:myPrint(aw:colorString("red", "   YOU HAVE VERY OLD AUCTIONS") );
+			aw:myPrint(aw:colorString("red", "*******************************************") );
+		end;
+	frame:UnregisterEvent("SPELLS_CHANGED");
+	end;
+	
+	if event == "PLAYER_LOGIN" then 
+		aw:VerifyDatabase()
+		aw.auctionCount = aw:GetCount(aw.ID)
+		--Single line report on login
+		if aw:ExpiredAuctions() then 
+			aw:myPrint(aw:colorString("red", "You have toons with auctions that need attention.") ); 	
+		else
+			aw:myPrint(aw:colorString("green", "No toons with auctions that need attention.") ); 	
+		end;
+		--Report to Window
+		if aw:GetWindow() then 
+			if aw:GetWindowOnlyOver() then
+				if  aw:ExpiredAuctions() then aw:ReportAuctionsToWindow(); end;
+			else
+				aw:ReportAuctionsToWindow(); 
+			end;
+		end;
+		--Report to chat
+		if aw:GetChat() then aw:ReportAuctionsToChat(); end;
+		aw.LoadOptions();
+	end 
+	
+	if event == "OWNED_AUCTIONS_UPDATED" then
+		aw.auctionCount = C_AuctionHouse.GetNumOwnedAuctions();
+	end
+	
+	if event == "AUCTION_HOUSE_AUCTION_CREATED"  then	
+		--Posting in the default UI doesn't call OWNED_AUCTIONS_UPDATED
+		--C_AuctionHouse.GetNumOwnedAuctions() isn't incremented here
+		--This workaround will count an extra auction if you post a commodity when you already have some posted
+		if AuctionHouseFrame.ItemSellFrame:IsVisible() == true then aw.auctionCount = aw.auctionCount + 1; end;
+	end
+		
+	if event == "AUCTION_HOUSE_CLOSED" then
+		if aw.auctionCount > 0 then	aw:UpdateToon(aw.ID); else aw:RemoveToonFromDB(aw.ID); end;
+	end
+end	
+frame:SetScript("OnEvent", frame.OnEvent);
+
+
+	
---
+++ AuctionWatch/Data.lua
@@ -0,0 +1,86 @@
+--aw namespace variable
+local _, aw = ...;
+
+
+function AuctionWatchGetAuctions()
+	if aw.auctionCount == nil then 
+		return 0;
+	else
+		return aw.auctionCount;
+	end
+end
+
+local isKeyValid = function (key)
+	--Returns true if aWatchDB.Settings[key] exists in the database
+	aw:VerifyDatabase()	--check if existing and add with default values if not
+	if aWatchDB.Settings[key] == nil then return false; else return true; end;
+end
+
+--*************************************************************************************
+--	Get settings from DB
+--*************************************************************************************
+function aw:GetChat() 			if isKeyValid("Chat") then return aWatchDB.Settings.Chat; end; end;
+function aw:GetWindow() 		if isKeyValid("Window") then return aWatchDB.Settings.Window; end; end;
+function aw:GetOnlyOver() 		if isKeyValid("OnlyOver") then return aWatchDB.Settings.OnlyOver; end; end; 
+function aw:GetDays() 	 		if isKeyValid("Days") then return aWatchDB.Settings.Days; end; end;
+function aw:GetAsc()      		if isKeyValid("Asc") then return aWatchDB.Settings.Asc; end; end;
+function aw:GetByDate()   	 	if isKeyValid("ByDate") then return aWatchDB.Settings.ByDate; end; end;
+function aw:GetWindowOnlyOver()	if isKeyValid("WinOnlyOver") then return aWatchDB.Settings.WinOnlyOver; end; end;
+function aw:GetSoundFlag()      if isKeyValid("PlaySound") then return aWatchDB.Settings.PlaySound; end; end;
+--*************************************************************************************
+--	Update settings
+--*************************************************************************************
+function aw:SetChat(value)				if isKeyValid("Chat") then aWatchDB.Settings.Chat = value; end; end;
+function aw:SetWindow(value)			if isKeyValid("Window") then aWatchDB.Settings.Window = value; end; end;
+function aw:SetOnlyOver(value)			if isKeyValid("OnlyOver") then aWatchDB.Settings.OnlyOver = value; end; end;
+function aw:SetDays(value)				if isKeyValid("Days") then aWatchDB.Settings.Days = value; end; end;
+function aw:SetAsc(value)				if isKeyValid("Asc") then aWatchDB.Settings.Asc = value; end; end;
+function aw:SetByDate(value)			if isKeyValid("ByDate") then aWatchDB.Settings.ByDate = value; end; end;
+function aw:SetWindowOnlyOver(value)	if isKeyValid("WinOnlyOver") then aWatchDB.Settings.WinOnlyOver = value; end; end;
+function aw:SetSoundFlag(value)	 		if isKeyValid("PlaySound") then aWatchDB.Settings.PlaySound = value; end; end;
+
+function aw:RemoveToonFromDB(t)
+	if aWatchDB.Auctions ~= nil then --delete this toon if there is no current auctions
+		if aWatchDB.Auctions[t] ~= nil then aWatchDB.Auctions[t] = nil; end;
+	end;
+end
+
+function aw:UpdateToon(t)
+	if aWatchDB.Auctions == nil then aWatchDB.Auctions = {}; end;	--create table if empty
+	aWatchDB.Auctions[t] = { ["count"] = aw.auctionCount; ["time"] = time() };
+end
+
+function aw:VerifyDatabase()
+	--check if DB exists  and create if not
+	if aWatchDB == nil then aWatchDB = {}; end;		
+	if aWatchDB.Settings == nil then	aw:DefaultSettings(); end;
+	if aWatchDB.Settings.PlaySound == nil then aWatchDB.Settings.PlaySound = true; end;
+end
+
+function aw:GetCount(t)
+	if t == nil then t = aw.ID; end;
+	--returns the number of auctions that toon 't' has listed (according to the database)
+	if aWatchDB.Auctions ~= nil then
+		if aWatchDB.Auctions[t] ~= nil then 
+			if aWatchDB.Auctions[t]["count"] ~= nil then 
+				return aWatchDB.Auctions[t]["count"]; 
+			end
+		end
+	end	
+	return 0;	--something was nil so return 0
+
+	
+end
+
+function aw:DefaultSettings()
+	aWatchDB.Settings = { 	
+		["Chat"] = false; 			--Don't list to chat
+		["OnlyOver"] = true; 		--Only list with expired auctions
+		["Window"] = true; 			--List to window(aw.Output frame)
+		["WinOnlyOver"] = true;		--Only show the frame if there are expired auctions
+		["Days"] = 2; 				--2 days from last AH visit before auctions are considered expired
+		["Asc"] = false; 			--Sort in descending order
+		["ByDate"] = true; 			--Sort by date
+		["PlaySound"] = true }; 	--Play Raid Warning when you have auctiond more then 25 days old
+end
+
